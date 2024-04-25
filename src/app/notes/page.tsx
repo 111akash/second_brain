6f8328414a8878a21c07b3b020f266ba4f8f98f2
akash <akash329@protonmail.com>
2024-04-25 17:23:23 +0530

    added clerk authentication

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "SecondBrain - Notes",
+}
+
+export default function NotePage() {
+    return <div>Here will be your notes</div>
+}
